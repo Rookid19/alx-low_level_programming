44f8130ddf903381f1e8304aa5386d9e86628797
rookid19 <randyodoom19@gmail.com>
2023-03-19 10:52:16 -0700

    with proper grammar, but the outcome is a piece of art,

---
+++ 0x00-hello_world/5-printf.c
@@ -0,0 +1,15 @@
+#include <stdio.h>
+
+/**
+* main - Entry point of code
+*
+* Description: prints a string with printf
+*
+* Return: Always 0 (success)
+*/
+
+int main(void)
+{
+	printf("%s\n","with proper grammar, but the outcome is a piece of art,");
+	return (0);
+}

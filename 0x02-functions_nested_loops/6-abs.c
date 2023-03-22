bba05deb324010d931ffcef99b8780a44c198a7f
rookid19 <randyodoom19@gmail.com>
2023-03-22 04:24:41 -0700

    function that computes the absolute value of an integer

---
+++ 0x02-functions_nested_loops/6-abs.c
@@ -0,0 +1,21 @@
+#include "main.h"
+
+/**
+* _abs - converts integer n to a positive number
+*
+* @n: number
+*
+* Return: absolute value
+*/
+
+int _abs(int n)
+	{
+	if (n >= 0)
+	{
+	return (n);
+	}
+	else
+	{
+	return (n * -1);
+	}
+}

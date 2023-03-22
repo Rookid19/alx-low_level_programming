79ea1472e74f05d35e164dc753a9d64ad91f3d7f
rookid19 <randyodoom19@gmail.com>
2023-03-21 19:49:23 -0700

    _putchar

--- 0x02-functions_nested_loops/0-putchar.c
+++ 0x02-functions_nested_loops/0-putchar.c
@@ -3,17 +3,20 @@
 /**
 * main - Print the word "_putchar"
 *
+* Description: prints a random number and evaluates if it's positive, negative
+* or zero
+*
 * Return: Always 0 (Success)
 */
 
 int main(void)
 {
 	int i;
-	char p[] = "_putchar";
+	char pArr[] = "_putchar";
 
 	for (i = 0; i < 8; i++)
 		{
-		_putchar(p[i]);
+		_putchar(pArr[i]);
 		}
 		_putchar('\n');
 	return (0);

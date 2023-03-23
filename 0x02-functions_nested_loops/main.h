31b60b779d95c6ed15a9361ffdc21d4af1443245
rookid19 <randyodoom19@gmail.com>
2023-03-22 21:01:27 -0700

    program that prints the first 50 Fibonacci numbers, starting with 1 and 2, followed by a new line

--- 0x02-functions_nested_loops/main.h
+++ 0x02-functions_nested_loops/main.h
@@ -1,8 +1,10 @@
+#ifndef MAIN_H_
+#define MAIN_H_
+
 int _putchar(char c);
 void print_alphabet(void);
 void print_alphabet_x10(void);
 int _islower(int c);
-int _isalpha(int c);
 int _isalpha(int c);
 int print_sign(int n);
 int _abs(int);
@@ -17,3 +19,5 @@ void pr_3_digit(int charcode, int fmt);
 void print_times_table(int n);
 void print_spaces(int n);
 void print_zero(void);
+
+#endif

b085acecec19472e93098cef6e02693ccf572d3c
rookid19 <randyodoom19@gmail.com>
2023-04-04 04:52:50 -0700

    function that locates a substring

--- 0x07-pointers_arrays_strings/5-strstr.c
+++ 0x07-pointers_arrays_strings/5-strstr.c
@@ -1,33 +1,35 @@
 #include "main.h"
 
 /**
- * _strpbrk - searches a string for any of a set of bytes
- * @s: String
- * @accept: Set of bytes
- * Return: A pointer to the byte in 's' that matches
- * one of the bytes in accept, or returns NULL
+ * _strstr - a function that locates a substring
+ * @haystack: locate a substring
+ * @needle: substring to locate
+ * Return: pointer to the beginning of the located substring,
+ * or returns NULL
  */
 
-char *_strpbrk(char *s, char *accept)
+char *_strstr(char *haystack, char *needle)
 {
-int i, j;
-char *p;
+	char *h = haystack;
+	char *n = needle;
 
-i = 0;
-while (s[i] != '\0')
-{
-j = 0;
-while (accept[j] != '\0')
-{
-if (accept[j] == s[i])
-{
-p = &s[i];
-return (p);
-}
-j++;
-}
-i++;
-}
-
-return (0);
+	while (*h)
+	{
+		n = needle;
+		h = haystack;
+		while (*n)
+		{
+			if (*h == *n)
+			{
+				n++;
+				h++;
+			}
+			else
+				break;
+		}
+		if (*n == '\0')
+			return (haystack);
+		haystack++;
+	}
+	return (0);
 }
